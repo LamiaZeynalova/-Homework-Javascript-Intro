19dd5885f99d5ac2e75298df42ee381a5061cf6c
LamiaZeynalova <lamiaaz@code.edu.az>
2024-02-05 08:26:11 +0400

    first commit

---
+++ task.js
@@ -0,0 +1,96 @@
+// /task1/
+// let num =4567;
+//  count=0;
+// for (let i=num;i>=1;i/=10) {
+//   count++;
+//  }
+//  if(count==4){
+//     console.log("7" + num + "8");
+//  }else{
+//     console.log("eded 4 reqemli deyil");
+//  }
+
+
+// task2//
+// for (let i = 1; i < 1000; i++) {
+//   let number = i;
+//   let sum = 0;
+//   while (number > 0) {
+//     sum += number % 10;
+//     number = (number - (number % 10)) / 10;
+//   }
+//   if (sum % 3 === 0) {
+//     console.log(i);
+//   }
+// }
+
+
+// let En = 100;
+// let H = 200;
+// let Uzs = 90;
+// let Hs = 250;
+// let Ens = 140;
+// if ( H > Hs && En > Uzs) {
+//   console.log("kecer");
+// } else if (En > Ens && H >Uzs) {
+//   console.log("kecer");
+// } else if (H > Uzs  && En > Hs) {
+//   console.log("kecer");
+// } else if (H >Ens && En >Uzs) {
+//   console.log("kecer");
+// } else if (En > Hs && H > Ens) {
+//   console.log("kecer");
+// } else if (En > Ensf && H > Hsf) {
+//   console.log("kecer");
+// } else {
+//   console.log("kecmez");
+// }
+
+
+// let Number=35;
+// let lastNumber = Number % 10;
+// switch (lastNumber) {
+//     case 1:
+//         console.log("I gun");
+//         break;
+//     case 2:
+//         console.log("II gun");
+//         break;
+//     case 3:
+//         console.log("III gun");
+//         break;
+//     case 4:
+//         console.log("IV gun");
+//         break;
+//     case 5:
+//         console.log("V gun");
+//         break;
+//     case 6:
+//         console.log("VI gun");
+//         break;
+//     case 7:
+//         console.log("VII gun");
+//         break;
+//     default:
+//         console.log("false");
+// }
+
+// let a = 4565;
+// // let b = 67854;
+// // if (a >= 1000 && a<=9999) {
+// //     sub1 = (a / 100) * 4
+// //     if (b >=1000000 && b<=9999999) {
+// //         sub2 = (b / 100) * 9
+// //         hesab = (sub1 + sub2) / 10
+// //         console.log(hesab);
+// //     }
+// //     else {
+//          console.log("a 4reqemli eded deyil");
+// //         
+// //     }
+// // }
+// // else {
+// //     console.log("b 7reqemli eded deyil");
+// // }
+
+ 
